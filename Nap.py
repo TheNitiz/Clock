14b61aa0db834e6d8cad2389fc1d5d1a751766ce
Nitiz <nitizjunior@gmail.com>
2022-01-13 12:44:12 +0530

    Nap File

--- Nap.py
+++ Nap.py
@@ -3,7 +3,7 @@ from playsound import playsound
 alarm_time = input("Enter the time of alarm to be set:HH:MM\n")
 alarm_hour=int(alarm_time[0:2])
 alarm_minute=int(alarm_time[3:5])
-#print("Alarm Set...")
+print("Alarm Set...")
 now = datetime.now()
 time_hour = now.hour+alarm_hour
 time_min = now.minute+alarm_minute
@@ -11,14 +11,13 @@ if time_min > 60:
     time_hour += time_min//60
     time_min = time_min%60
 if time_hour > 23:
-    time_hour = time_hour//24
-n=1    
-while n==1 and len(alarm_time) == 5:
+    time_hour = time_hour//24    
+while True and len(alarm_time) == 5:
     now = datetime.now()
     hour = now.hour
     minute = now.minute
     if(time_hour==hour):
         if(time_min==minute):
             print("Wake Up!")
-            playsound("C:/Users/DELL/VScode/PythonProjects/Ringtone.mp3")
+            playsound("C:/Users/DELL/VScode/PythonProjects/Clock/Ringtone.mp3")
             break
