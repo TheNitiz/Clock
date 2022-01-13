e545f3d4b474f9a2b441688314c4867a936990f8
Nitiz <nitizjunior@gmail.com>
2022-01-13 11:37:13 +0530

    ALL files commit for first time

---
+++ Alarm.py
@@ -0,0 +1,23 @@
+from datetime import datetime   
+from playsound import playsound
+alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
+alarm_hour = alarm_time[0:2]
+alarm_minute = alarm_time[3:5]
+alarm_seconds = alarm_time[6:8]
+alarm_period = alarm_time[9:11].upper()
+print("Setting up alarm..")
+while True and len(alarm_time) == 11:
+    now = datetime.now()
+    current_hour = now.strftime("%I")
+    current_minute = now.strftime("%M")
+    current_seconds = now.strftime("%S")
+    current_period = now.strftime("%p")
+    if(alarm_period==current_period):
+        if(alarm_hour==current_hour):
+            if(alarm_minute==current_minute):
+                if(alarm_seconds==current_seconds):
+                    print("Wake Up!")
+                    playsound("C:/Users/DELL/VScode/PythonProjects/Clock/Ringtone.mp3")
+                    break
+else:
+    print("Please Enter the time in Valid Format")
---
+++ Nap.py
@@ -0,0 +1,24 @@
+from datetime import datetime   
+from playsound import playsound
+alarm_time = input("Enter the time of alarm to be set:HH:MM\n")
+alarm_hour=int(alarm_time[0:2])
+alarm_minute=int(alarm_time[3:5])
+#print("Alarm Set...")
+now = datetime.now()
+time_hour = now.hour+alarm_hour
+time_min = now.minute+alarm_minute
+if time_min > 60:
+    time_hour += time_min//60
+    time_min = time_min%60
+if time_hour > 23:
+    time_hour = time_hour//24
+n=1    
+while n==1 and len(alarm_time) == 5:
+    now = datetime.now()
+    hour = now.hour
+    minute = now.minute
+    if(time_hour==hour):
+        if(time_min==minute):
+            print("Wake Up!")
+            playsound("C:/Users/DELL/VScode/PythonProjects/Ringtone.mp3")
+            break
